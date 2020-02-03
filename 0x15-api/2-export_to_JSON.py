2b31c512d665b7f1296f8b2c35d8fff4f6adb712
hacheG <943@holbertonschool.com>
2020-02-03 18:09:43 -0500

    [FIX] sin huecos

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""a day more for die"""
-
+"""
+a day more for die
+"""
 import requests
 from sys import argv
 import json
-
 if __name__ == "__main__":
-
     theNumId = argv[1]
-
     r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                      .format(theNumId))
     showJson = r.json()
     theUserName = showJson.get('username')
-
     r2 = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
                       .format(theNumId))
     showJson2 = r2.json()
-
     listToJson = []
     theNumId = int(theNumId)
     for js in showJson2:
@@ -29,8 +25,6 @@ if __name__ == "__main__":
         dictToJson["task"] = theTitle
         dictToJson["completed"] = completedTF
         listToJson.append(dictToJson)
-
     dict_2_Json = {theNumId: listToJson}
-
     with open('{}.json'.format(theNumId), "w", encoding="utf-8") as j:
         json.dump(dict_2_Json, j)
