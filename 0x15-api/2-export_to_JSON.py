08eb09070579d05211400b13ec7af50489babf86
hacheG <943@holbertonschool.com>
2020-02-03 15:10:46 -0500

    [FEATURE] add task2 & json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""a day more for die"""
+
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+
+    numTaskCompĺete = 0
+    theNumId = argv[1]
+    
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(theNumId))
+    showJson = r.json()
+    theUserName = showJson.get('username')
+
+    r2 = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(theNumId))
+    showJson2 = r2.json()
+
+    listToJson = []
+    theNumId = int(theNumId)
+    for js in showJson2:
+        dictToJson = {}
+        theUserId = js.get('userId')
+        completedTF = js.get('completed')
+        theTitle = js.get('title')
+        dictToJson["username"] = theUserName
+        dictToJson["task"] = theTitle
+        dictToJson["completed"] = completedTF
+        listToJson.append(dictToJson)
+    
+    dict_2_Json = {theNumId : listToJson}
+
+    with open('{}.json'.format(theNumId), "w") as j:
+        json.dump(dict_2_Json, j)
