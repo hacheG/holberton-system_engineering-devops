e1d0c5a9f86757ef265f6bc349035c69e3896bb7
hacheG <943@holbertonschool.com>
2020-02-06 19:08:06 -0500

    [FEATURE] add search in api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""i see API's everywhere"""
+import requests
+
+
+def top_ten(subreddit):
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        for req in range(10):
+            dataJson = r.json()['data']['children'][req]['data']['title']
+            print (dataJson)
+    else:
+        print (None)
