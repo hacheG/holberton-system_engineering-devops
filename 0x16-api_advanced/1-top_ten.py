d534380a6c4f38a888f57602624d34047200f333
hacheG <943@holbertonschool.com>
2020-02-06 19:53:15 -0500

    [FIX] variable

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,11 +4,12 @@ import requests
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    final = 10
+    url = 'https://www.reddit.com/r/{}/hot.json?limit={}'.format(subreddit, final)
     headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
-        for req in range(10):
+        for req in range(final):
             dataJson = r.json()['data']['children'][req]['data']['title']
             print (dataJson)
     else:
