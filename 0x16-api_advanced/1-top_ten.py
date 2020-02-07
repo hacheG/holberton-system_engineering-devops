9bf7e34cd41cae1961c4a0111f755a39886096d0
hacheG <943@holbertonschool.com>
2020-02-06 20:20:10 -0500

    [FIX] change range(10)

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """i see API's everywhere"""
+import pprint
 import requests
+
+BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        dataJson = r.json()['data']['children']
-        counter = 0
-        for req in dataJson:
-            if counter == 10:
-                break
-            print (req['data']['title'])
-            counter = counter + 1
-    else:
-        print ("None")
+    '''Get top 10 hot posts'''
+    headers = {'User-agent': 'Unix:0-subs:v1'}
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers=headers)
+    data = response.json().get('data', {}).get('children', {})
+    if response.status_code != 200 or not data:
+        return print("None")
+    for post in data[0:10]:
+        print(post.get('data', {}).get('title'))
