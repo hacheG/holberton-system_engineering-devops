e48b2472e7dd19d8a994b79e1a0a8e93377cceb3
hacheG <943@holbertonschool.com>
2020-02-06 20:02:01 -0500

    [FIX] change range(10)

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,8 +9,9 @@ def top_ten(subreddit):
     headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
-        for req in range(final):
-            dataJson = r.json()['data']['children'][req]['data']['title']
-            print (dataJson)
+        dataJson = r.json()['data']['children']
+        for req in dataJson:
+            dataTitle = req['data']['title']
+            print (dataTitle)
     else:
         print ("None")
